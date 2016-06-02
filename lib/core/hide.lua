2d5ace21a31351f525cff415bf345b869c21e11e
powerboat9 <powerboat9@users.noreply.github.com>
2016-06-02 17:37:38 -0400

    Update hide.lua

--- lib/core/hide.lua
+++ lib/core/hide.lua
@@ -4,4 +4,7 @@ function hide(file)
     if fs.ge
     hidden[file] = true
     if fs.exists(file) then
-        local fList = fs.list(file:sub(-
+        local fList = fs.list(file:sub(1, -2 - #fs.getName(file)), true)
+        local repList = {}
+        for _, v in ipairs(fList) do
+            if 
