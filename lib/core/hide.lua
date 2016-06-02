60e9f5c199c83ed08f4436c69df7aefaad969532
powerboat9 <powerboat9@users.noreply.github.com>
2016-06-02 18:32:37 -0400

    Update hide.lua

--- lib/core/hide.lua
+++ lib/core/hide.lua
@@ -1,10 +1,22 @@
 local hidden = {}
 
+local function appendPadding(path, nPadding)
+    local name = fs.getName(path)
+    local dir = path:sub(1, -2 - #name)
+    return dir .. ("~"):rep(nPadding) .. name
+end
+
 function hide(file)
     if fs.ge
     hidden[file] = true
+    local numPadCycles = 0
+    while true do
+        if fs.exists(
     if fs.exists(file) then
         local fList = fs.list(file:sub(1, -2 - #fs.getName(file)), true)
         local repList = {}
+        local nPadCycles = 0
         for _, v in ipairs(fList) do
-            if 
+            local _, _, pad, name = fs.getName(v):find("^(~*)(.*)$")
+            pad = #pad
+            repList[pad] = name
