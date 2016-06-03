7a229b43721e76eccead728ec19407c903ca5499
powerboat9 <powerboat9@users.noreply.github.com>
2016-06-02 20:01:14 -0400

    Update hide.lua

--- lib/core/hide.lua
+++ lib/core/hide.lua
@@ -9,14 +9,20 @@ end
 function hide(file)
     if fs.ge
     hidden[file] = true
-    local numPadCycles = 0
-    while true do
-        if fs.exists(
     if fs.exists(file) then
         local fList = fs.list(file:sub(1, -2 - #fs.getName(file)), true)
-        local repList = {}
-        local nPadCycles = 0
+        local repList, parse = {}, {}
         for _, v in ipairs(fList) do
             local _, _, pad, name = fs.getName(v):find("^(~*)(.*)$")
             pad = #pad
-            repList[pad] = name
+            repList[#repList + 1] = {pad, name}
+            parse[pad] = true
+        end
+        local function adderThing(n, sum)
+            if n < 0 then
+                return sum
+            else
+                return adderThing(n - 1, sum + n)
+            end
+        end
+        for i = 1, adderThing(repList)
